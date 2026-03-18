48695667faaedb9f766e4d19950bca1da38f56bd
Adam Koplinger <xkoplinger@stuba.sk>
2026-03-18 22:38:17 +0100

    Prva cast

--- build/CMakeFiles/robot_logic.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "librobot_logic.a"
-)
--- build/CMakeFiles/robot_logic.dir/src/robot/Robot.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/robot_logic.dir/src/robot/Robot.cpp.o: \
- /home/adam/robot_projekt/src/robot/Robot.cpp /usr/include/stdc-predef.h
